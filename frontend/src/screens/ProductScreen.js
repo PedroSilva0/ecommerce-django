1099f94d6a32e2e6fd52c72239be4d6777d1017d
Pedro Silva <pedrocas-silva@hotmail.com>
2022-02-02 21:09:32 +0000

    Created Django backend Connected backend to frontend Created database models in Django

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 import {
   Row,
@@ -11,11 +12,21 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 function ProductScreen(props) {
   const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct(id) {
+      const { data } = await axios.get("api/products/" + id);
+      setProduct(data);
+    }
+
+    fetchProduct(id);
+  }, []);
+
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
